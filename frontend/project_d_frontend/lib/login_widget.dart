d69a445e0c1e42fe0d3b34f958847f76e19bcb3b
Daan <Daaandeheer@gmail.com>
2020-05-11 20:40:25 +0200

    Progress bar

--- frontend/project_d_frontend/lib/login_widget.dart
+++ frontend/project_d_frontend/lib/login_widget.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 
 class LoginWidget extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => new _LoginWidget();
+}
 
+Widget showCircularProgess(){
+  bool _isLoading;
+  if (_isLoading){
+    return Center(child: CircularProgressIndicator());
+  }
+  return Container(
+    height: 0.0,
+    width: 0.0,
+  );
 }
 
 
-class _LoginWidget extends State<LoginWidget>{
 
-
+class _LoginWidget extends State<LoginWidget> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Flutter login demo"),
-      ),
-      body: new Container(
-        child: new Text("Hello World"),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text("Flutter login demo"),
+        ),
+        body: Stack(
+          children: <Widget>[
+            showForm(),
+            showCircularProgress(),
+          ],
+        ));
   }
 }
