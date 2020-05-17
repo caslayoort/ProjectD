14e542448eeb6c3b59a15c7116a7fb104a04053f
Daan <Daaandeheer@gmail.com>
2020-05-17 21:39:47 +0200

    added email and password textfield

--- frontend/project_d_frontend/lib/login_widget.dart
+++ frontend/project_d_frontend/lib/login_widget.dart
@@ -1,52 +1,56 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
-
-
 //https://flutter.dev/docs/cookbook/forms/validation
 class LoginWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _LoginWidget();
-
-
 }
 
-
-
-
-
 class _LoginWidget extends State<LoginWidget> {
+  String _email, _password;
 
-  String _email;
-  
-  Widget email(){
+  Widget email() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0) ,
+      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
       child: TextFormField(
         keyboardType: TextInputType.emailAddress,
-        validator: EmailValidator.validate(email) ,
+        //validator:
         onSaved: (value) => _email = value.trim(),
-        
-      ) ,
-      
-      
-      );
+        decoration: InputDecoration(
+          hintText: 'Email',
+        ),
+      ),
+    );
+  }
 
-    
-  } 
-  
-  
-  
+    Widget password() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+      child: TextFormField(
+        keyboardType: TextInputType.visiblePassword,
+        //validator:
+        onSaved: (value) => _password = value.trim(),
+        decoration: InputDecoration(
+          hintText: 'Wachtwoord',
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("Flutter login demo"),
         ),
-        body: Stack(
-          children: <Widget>[
-            
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            email(),
+            password(),
           ],
+          
         ));
   }
 }
