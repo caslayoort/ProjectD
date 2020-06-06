5ae8432e1e1b56f11f4b0864fa15f8dc0fbf652e
Daan <Daaandeheer@gmail.com>
2020-06-06 19:10:57 +0200

    firebase_auth finally working

--- frontend/project_d_frontend/lib/login_widget.dart
+++ frontend/project_d_frontend/lib/login_widget.dart
@@ -1,72 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:project_d_frontend/Setup/auth.dart';
 
 class LoginWidget extends StatefulWidget {
+  LoginWidget({this.auth});
+
+  final BaseAuth auth;
+
   @override
   State<StatefulWidget> createState() => new _LoginWidget();
 }
 
 class _LoginWidget extends State<LoginWidget> {
-  String _email, _password;
-  bool _isLoginForm = true;
+  final _formKey = new GlobalKey<FormState>();
 
-  void check() {
-    print('Pressed button');
+  String _email, _password;
+  String _errorMessage;
+  bool _isLoginForm;
+
+   bool validateAndSave() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void validateAndSubmit() async {
+    setState(() {
+      _errorMessage = "";
+      
+    });
+    if (validateAndSave()) {
+      String userId = "";
+      try {
+        if (_isLoginForm) {
+          userId = await widget.auth.signIn(_email, _password);
+          print('Signed in: $userId');
+        } else {
+          userId = await widget.auth.signUp(_email, _password);
+          print('Signed up user: $userId');
+        }
+      } catch (e) {
+        print('Error: $e');
+        setState(() {
+          _errorMessage = e.message;
+          _formKey.currentState.reset();
+        });
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    _errorMessage = "";
+    _isLoginForm = true;
+    super.initState();
   }
 
   Widget logo() {
-   return Image(
+    return Image(
       image: AssetImage('images/logo.png'),
-      width: 300,
-      height: 200,
-      fit: BoxFit.cover,  
+      width: 325,
+      height: 250,
+      
+      fit: BoxFit.cover,
     );
   }
 
   Widget email() {
     return Padding(
       padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
-      child: TextFormField(
+      child: new TextFormField(
+        maxLines: 1,
         keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          hintText: 'Email',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(32.0),
-            borderSide: BorderSide(color: Colors.blue),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(32.0),
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-        ),
-        validator: (value) => value.isEmpty ? "Email can/'t be empty" : null,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Email',
+            icon: new Icon(
+              Icons.mail,
+              color: Colors.blue[900],
+            )),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
         onSaved: (value) => _email = value.trim(),
       ),
     );
   }
 
   Widget password() {
-    return TextFormField(
-      keyboardType: TextInputType.visiblePassword,
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Wachtwoord',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32.0),
-          borderSide: BorderSide(color: Colors.blue),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32.0),
-          borderSide: BorderSide(color: Colors.grey),
-        ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Password',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.red[900],
+            )),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value.trim(),
       ),
-      validator: (value) => value.isEmpty ? "Password can/'t be empty" : null,
-      onSaved: (value) => _password = value.trim(),
     );
   }
 
-  Widget loginbutton() {
+  Widget signupbutton() {
     return new Padding(
         padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
         child: SizedBox(
@@ -75,10 +115,67 @@ class _LoginWidget extends State<LoginWidget> {
             elevation: 5.0,
             shape: new RoundedRectangleBorder(
                 borderRadius: new BorderRadius.circular(30.0)),
-            color: Colors.blue,
+            color: Colors.red[800],
             child: new Text(_isLoginForm ? 'Login' : 'Create account',
                 style: new TextStyle(fontSize: 20.0, color: Colors.white)),
-            onPressed: check, //validateAndSubmit
+            onPressed: validateAndSubmit
+          ),
+        ));
+  }
+
+  Widget loginbutton() {
+    return new FlatButton(
+        child: new Text(
+            _isLoginForm ? 'Create an account' : 'Have an account? Sign in',
+            style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300)),
+        onPressed: changeform);
+  }
+
+  void resetForm() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+  }
+
+  void changeform() {
+    resetForm();
+    setState(() {
+      _isLoginForm = !_isLoginForm;
+    });
+  }
+
+  Widget showerror() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return new Text(
+        _errorMessage,
+        style: TextStyle(
+            fontSize: 13.0,
+            color: Colors.red,
+            height: 1.0,
+            fontWeight: FontWeight.w300),
+      );
+    } else {
+      return new Container(
+        height: 0.0,
+      );
+    }
+  }
+
+  Widget _showForm() {
+    return new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+          key: _formKey,
+          child: new ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              SizedBox(height: 10),
+              logo(),
+              email(),
+              password(),
+              signupbutton(),
+              loginbutton(),
+              showerror()
+            ],
           ),
         ));
   }
@@ -86,18 +183,9 @@ class _LoginWidget extends State<LoginWidget> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Voice assistant"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 50),
-            logo(),
-            email(),
-            SizedBox(height: 10),
-            password(),
-            loginbutton(),
+        body: Stack(
+          children: <Widget>[
+            _showForm(),
           ],
         ));
   }
--- frontend/project_d_frontend/lib/main.dart
+++ frontend/project_d_frontend/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_d_frontend/login_widget.dart';
 
+
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,10 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Voice assistant',
-      theme: new ThemeData(
-        primarySwatch: Colors.red
-      ),
       home: new LoginWidget()
     );
   }
