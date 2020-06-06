e2788834a3316db11d56d54ef1c5c8b6d1d28e1c
caslayoort <root@localhost.localdomain>
2020-06-06 15:52:44 +0200

    added code infra

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ProjectD
+
+
+## Api
+The api is a nodejs api that calls a python script that will make the answer.
+
+To use the api you need to have the latest nodejs and python3 installed with the nltk library installed using pip3.
--- api/python/main.py
+++ api/python/main.py
@@ -1,6 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import json
+import sys
+
+sys.path.append('./phone.py')
+sys.path.append('./subscription.py')
+
+import phone as phone
+import subscription as sub
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+def getResponse(intent, question, defaultAnswer, endOfConversation):
+    en_stops = set(stopwords.words('english'))
+    all_words = list(question.split(" "))
+    filteredQuestion = ""
+    for word in all_words: 
+        if word not in en_stops:
+            filteredQuestion = filteredQuestion + " " + word
+
+    PhoneAnswer = phone.checkIfPhone(intent, question)
+    if PhoneAnswer:
+        return PhoneAnswer
+    subscriptionAnswer = sub.checkIfSubscription(intent, question)
+    if subscriptionAnswer:
+        return subscriptionAnswer
+    return(defaultAnswer)
 
 if __name__ == "__main__":
     timestamp = str(sys.argv[1])
@@ -18,6 +45,8 @@ if __name__ == "__main__":
     intentName = myjson["queryResult"]["intent"]["displayName"]
     endOfConversation = myjson["queryResult"]["intent"]["displayName"]
 
+    answer = getResponse(intentName, question, defaultAnswer, endOfConversation)
+
     f2 = open(jsonOutputFile, 'w+')
-    f2.write(question);
+    f2.write(answer)
     f2.close()
