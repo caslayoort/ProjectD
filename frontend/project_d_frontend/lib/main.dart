3c8468f040c40402c38156f5b77e49d3a71b49b7
Daan <Daaandeheer@gmail.com>
2020-06-08 23:21:47 +0200

    working credentials

--- frontend/project_d_frontend/lib/main.dart
+++ frontend/project_d_frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_d_frontend/Setup/auth.dart';
+import 'package:project_d_frontend/home_page.dart';
 import 'package:project_d_frontend/login_widget.dart';
 
 
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new LoginWidget(auth: new Auth(),)
+      home: new HomePageDialogflow()
+      //new LoginWidget(auth: new Auth(),)
     );
   }
 }
