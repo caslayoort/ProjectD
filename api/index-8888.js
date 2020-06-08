c65a0efce92f9a11347e59261c4684fc280357c6
caslayoort <root@localhost.localdomain>
2020-06-08 07:55:10 +0200

    merged with laurens's code

--- api/index-8888.js
+++ api/index-8888.js
@@ -11,6 +11,18 @@ const port = 8888;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
+global.parameters;
+global.outputcontexts;
+global.action;
+global.fulfillmentmessages;
+
+global.localparameters;
+global.localoutputcontexts;
+global.localaction;
+global.localfulfillmentMessages
+
+// Caslay code
+
 // Make sure Google is able to access it. Basicly setting permissions.
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -28,7 +40,7 @@ const db = firebaseadmin.firestore();
 
 
 // Response function. Makes a valid json to send to firebase.
-const responsejson = (resp) => {
+const responsejsonCaslay = (resp) => {
   textfulfillment = '{"fulfillmentMessages": [{ "text": { "text": [ "' + resp + '" ] } } ] }';
   return textfulfillment;
 };
@@ -59,6 +71,146 @@ const logreq = (r) => {
   }
 };
 
+// Laurens code
+
+const responsejson = (resp) => {
+  let textfulfillment = '{"fulfillmentText": "' + resp + '"'
+  if ( typeof global.parameters != 'undefined'){
+      textfulfillment = textfulfillment+ ', "parameters": ['
+      textfulfillment = textfulfillment + JSON.stringify(global.parameters);
+      textfulfillment = textfulfillment + ']';
+  }
+  if ( typeof global.fulfillmentmessages != 'undefined'){
+      textfulfillment = textfulfillment+ ', "fulfillmentMessages": ['
+      textfulfillment = textfulfillment + JSON.stringify(global.fulfillmentmessages);
+      textfulfillment = textfulfillment + ']';
+  }
+  
+  textfulfillment =textfulfillment + ', "queryText": "' + "lukt" + '"' + '}';
+  return textfulfillment;
+};
+       
+function webhook() {
+  var date = new Date();
+  global.localparameters = { leggo: date.toJSON() };
+  return "works ";
+}        
+      
+function webhookfollowup() {
+  return  "date : " +  new Date(global.localparameters["leggo"]).toString();
+ 
+}        
+function appointmentfollowup() {
+    if (typeof global.localaction != 'undefined'){
+        return new Date(global.localaction["datedate"]).toString();
+    }
+    else {
+    return "please try again"
+    }
+}   
+function appointment() {
+    var list = [];
+    var answer = '';
+    var date = new Date();
+     for (var key in global.parameters) {
+         if(key == 'otherdays' ){
+             let temp  = global.parameters[key];
+             if(temp  ==  'tomorow' ){
+               date.setDate(date.getDate() + 1);
+               
+             }else if(temp == 'next week'){
+                date.setDate(date.getDate() + 7);
+             }
+         }
+         else if(key == 'days' && global.parameters[key] != ''){
+           var a = 0;
+           var b = global.parameters[key];
+           
+           switch(b) {
+                case "monday" || "Monday": 
+                  a=1
+                  break;
+                 case "tuesday" || "Tuesday": 
+                  a=2
+                  break;
+                 case "wednesday" || "Wednesday": 
+                  a=3
+                  break;
+                 case "thursday" || "Thursday": 
+                  a=4
+                  break;
+                  case "friday" || "Friday": 
+                  a=5
+                  break;
+                   case "saturday" || "Saturday": 
+                  a=6
+                  break;
+                  case "sunday" ||"Sunday":  
+                  a=7
+                  break;
+                default: 
+                  return "try again?";
+            }
+                        
+            var n = date.getDay();
+            if(n > a){
+               date.setDate(date.getDate() + 7-n+a);
+            }else if(n < a){
+              date.setDate(date.getDate() + 7-a);
+            }
+            else if(n == a){
+              date.setDate(date.getDate() + 7);
+            }
+         }
+         else if(key = 'time'){
+           if(global.parameters[key] < 18 && global.parameters[key] >8 ){
+             date.setHours(global.parameters[key]);  
+             date.setMinutes(0);  
+             date.setSeconds(0);
+           } 
+           else if(global.parameters[key] < 5){
+             date.setHours(global.parameters[key] +12);  
+             date.setMinutes(0);  
+             date.setSeconds(0);
+           }
+        }
+      
+      
+    }
+    //{ "name":"John", "age":30, "car":null }
+    global.localaction = { datedate: date.toJSON() };
+    console.log(global.localaction)
+    return  String(date);
+}
+
+const getResponse = () => {
+    if (typeof global.action != 'undefined') {
+        switch (action) {
+            case "webhookdo":
+                return webhook()
+                break;
+            case "webhookdoagain":
+                return webhookfollowup()
+                break;
+            case "appointment.appointment-yes.appointment-yes-custom":
+                return 'Confirm this appointment: ' + appointment();
+                break;
+            case "finalappointment":
+                return 'great we will see you on: ' + appointmentfollowup();
+                break;
+            case "wantstolocation":
+                return "let me calculate that for you"
+                break;
+        }
+    }
+    else {
+        return "hello"
+    }
+};
+
+
+
+
 // Receives the request from google and handles it.
 app.post('/webhook', (req, res) => {
   function sleep(ms) {
@@ -74,31 +226,48 @@ app.post('/webhook', (req, res) => {
 
   if ( typeof data !== 'undefined' && data ){
     let question = data.queryResult.queryText;
-    
+    const timestamp = logreq(data);
     console.log("\nQuestion:\n" + question);
 
-    const timestamp = logreq(data);
-    const { spawn } = require('child_process');
-    
-    // spawns a subproces that takes the timestamp of the request and the current directory to get the request file that is created.
-    const child = spawn(__dirname + '/python/main.py', [`${timestamp}`, __dirname]);
+    global.action = data.queryResult.action
 
-    // create a period of time for the sub process to run to avoid returning "undefined" as our result.
-    let waitForIt = sleep(3000);
-    waitForIt.then(function(){
-      
-    if(child.error) {
-      console.log(child.error);
-      res.send("Something went wrong! Please try again.")
+    // Laurens request
+    if (typeof global.action !== 'undefined' && global.action && global.action !== "input.unknown"){
+      global.question = data.queryResult.queryText;
+      global.parameters = data.queryResult.parameters;
+      global.outputcontexts =  data.queryResult.outputContexts;
+      global.fulfillmentmessages = data.queryResult.fulfillmentmessages;
+      let response = getResponse();
+     
+      console.log("\nParameters After:\n" +  JSON.stringify(global.parameters));
+      console.log("\nResponse: " +responsejson(response))
+      res.send(responsejson(response))
     }
+
+    // Caslay request
     else {
-      // Getting the response from the response file
-      fs.readFile( __dirname + '/log/responses/' + timestamp + '.log', 'utf8', function (err, data) {
-           console.log("\nAnswer:\n" + data);
-           res.send(responsejson(data));
-       });
-     }
-    });
+      const { spawn } = require('child_process');
+      
+      // spawns a subproces that takes the timestamp of the request and the current directory to get the request file that is created.
+      const child = spawn(__dirname + '/python/main.py', [`${timestamp}`, __dirname]);
+
+      // create a period of time for the sub process to run to avoid returning "undefined" as our result.
+      let waitForIt = sleep(3000);
+      waitForIt.then(function(){
+        
+      if(child.error) {
+        console.log(child.error);
+        res.send("Something went wrong! Please try again.")
+      }
+      else {
+        // Getting the response from the response file
+        fs.readFile( __dirname + '/log/responses/' + timestamp + '.log', 'utf8', function (err, data) {
+            console.log("\nAnswer:\n" + data);
+            res.send(responsejson(data));
+        });
+      }
+      });
+    }
   } else {
     res.send(responsejson("Something went wong! please try again."));
   }
