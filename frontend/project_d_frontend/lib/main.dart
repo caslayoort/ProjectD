6afd1dbc95043af51d4f73f9c8271de5d04b033d
Daan <Daaandeheer@gmail.com>
2020-06-10 13:18:42 +0200

    changed main

--- frontend/project_d_frontend/lib/main.dart
+++ frontend/project_d_frontend/lib/main.dart
@@ -9,8 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        home: new HomePage()
-        //new LoginWidget(auth: new Auth())
+        home: new LoginWidget(auth: new Auth())
 
 
     );
